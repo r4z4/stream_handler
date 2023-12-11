d651f7e4020310cc5b3a313103079e1a3736ec3d
r4z4 <r4z4aa@gmail.com>
2023-12-10 21:12:50 -0600

    release file

---
+++ lib/stream_handler/release.ex
@@ -0,0 +1,26 @@
+defmodule StreamHandler.Release do
+  @app :stream_handler
+
+  def migrate do
+    ensure_started()
+
+    for repo <- repos() do
+      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+    end
+  end
+
+  def rollback(repo, version) do
+    ensure_started()
+
+    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+  end
+
+  defp ensure_started do
+    Application.ensure_all_started(:ssl)
+  end
+
+  defp repos do
+    Application.load(@app)
+    Application.fetch_env!(@app, :ecto_repos)
+  end
+end
